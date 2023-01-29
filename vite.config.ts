abc367a4c1234b355486dcb188cbc2a2e9c9fb75
David Oquendo <davidoqu23@gmail.com>
2023-01-29 11:47:49 -0600

    refactored vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,12 @@ import content from '@originjs/vite-plugin-content'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [checker({ typescript: true }), content(), react()],
+  plugins: [
+    checker({ typescript: true }), 
+    content({
+      yaml: { enabled: true },
+      csv: { enabled: true },
+    }), 
+    react()
+  ],
 })
