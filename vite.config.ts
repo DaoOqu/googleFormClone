33005fd3dc34a9629d8e22de0e665c67275d249d
David Oquendo <davidoqu23@gmail.com>
2023-02-01 18:52:57 -0600

    test commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ import content from '@originjs/vite-plugin-content'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [checker({ typescript: true }), (content as any).default(), react()],
+  plugins: [
+    checker({ typescript: true }), 
+    (content as any).default(), 
+    react()
+  ],
 })
