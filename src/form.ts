f8248515fce10c1d970fd553b12d80d3258e5e63
David Oquendo <davidoqu23@gmail.com>
2023-02-04 11:09:14 -0600

    Added form question data types

--- src/form.ts
+++ src/form.ts
@@ -6,8 +6,18 @@ const nanoid = customAlphabet(
 )
 
 export type FormId = string;
+export type ChoiceId = string;
+export type QuestionId = string;
 
 export const generateFormId = (): FormId => {
+  return nanoid()
+}
+
+export const generateQuestionId = (): FormId => {
+  return nanoid()
+}
+
+export const generateChoiceId = (): FormId => {
   return nanoid()
 }
 
@@ -15,4 +25,54 @@ export type Form = {
   id: FormId
   name: string
   published: boolean
+  questions: Array<FormQuestion>
+}
+
+export type FormQuestion =
+  | { tag: 'shorText'; question: ShortTextQuestion }
+  | { tag: 'longText'; question: LongTextQuestion }
+  | { tag: 'singleChoice'; question: SingleChoiceQuestion }
+  | { tag: 'multipleChoice'; question: MultipleChoiceQuestion }
+  | { tag: 'scale'; question: ScaleQuestion }
+
+export type ShortTextQuestion = {
+  title: string
+  description: string | null
+  required: boolean
+}
+
+export type LongTextQuestion = {
+  title: string
+  description: string | null
+  required: boolean
+}
+
+export type SingleChoiceQuestion = {
+  title: string
+  description: string | null
+  required: boolean
+  choices: Array <{
+    id: ChoiceId
+    value: string
+  }>
+}
+
+export type MultipleChoiceQuestion = {
+  title: string
+  description: string | null
+  required: boolean
+    choices: Array <{
+    id: ChoiceId
+    value: string
+  }>
+}
+
+export type ScaleQuestion = {
+  title: string
+  description: string | null
+  required: boolean
+  start: number
+  end: number
+  startLabel: string
+  endLabel: string
 }
